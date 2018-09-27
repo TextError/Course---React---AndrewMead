5c1f2e45e3a628f2eb01d60fca9d2fb418dd9b2a
unknown <savinraul1983@yahoo.com>
2018-09-27 11:33:51 +0100

    $ babel src/app.js --out-file=public/scripts/app.js --presets=env,react

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,13 @@
+'use strict';
+
 console.log('app');
 
 //JSX  JavaScript XML
-var template = <p>This is JSX from app.js!</p>
+var template = React.createElement(
+  'p',
+  null,
+  'This is JSX from app.js!'
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
