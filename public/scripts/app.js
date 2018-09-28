eb3e1cecd8b6f08de1acca9c56b25e634aa91294
unknown <savinraul1983@yahoo.com>
2018-09-28 11:46:52 +0100

    let and const

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,25 @@
 'use strict';
 
 var nameVar = 'Andrew';
+var nameVar = 'Mike';
 
 console.log('nameVar', nameVar);
+
+var nameLet = 'Jen';
+nameLet = 'Julie';
+// let nameLet = 'Julie'; Duplicate declaration "nameLet"
+
+console.log('nameLet', nameLet);
+
+var nameConst = 'Frank';
+// const nameConst = 'Ghunter'; Duplicate declaration "nameConst"
+// nameConst = 'Ghunter' value does not change
+
+console.log('nameConst', nameConst);
+
+function getPetName() {
+  var petName = 'Hal';
+  return petName;
+}
+
+getPetName();
--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -1,3 +1,23 @@
 var nameVar = 'Andrew';
+var nameVar = 'Mike';
 
 console.log('nameVar', nameVar);
+
+let nameLet = 'Jen';
+nameLet = 'Julie';
+// let nameLet = 'Julie'; Duplicate declaration "nameLet"
+
+console.log('nameLet', nameLet);
+
+const nameConst = 'Frank';
+// const nameConst = 'Ghunter'; Duplicate declaration "nameConst"
+// nameConst = 'Ghunter' value does not change
+
+console.log('nameConst', nameConst);
+
+function getPetName() {
+  var petName= 'Hal';
+  return petName
+}
+
+getPetName();
