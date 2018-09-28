69927730ddc2c348ebbfc79e6f19f382d703f3ca
unknown <savinraul1983@yahoo.com>
2018-09-28 11:04:28 +0100

    logical AND operator

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -48,12 +48,14 @@ var templateTwo = React.createElement(
     null,
     user.name ? user.name : 'Anonymous'
   ),
-  React.createElement(
+  ' ',
+  user.age && user.age >= 18 && React.createElement(
     'p',
     null,
     'Age: ',
     user.age
   ),
+  ' ',
   getLocation(user.location)
 );
 
--- src/app.js
+++ src/app.js
@@ -28,8 +28,8 @@ function getLocation(location) {
 
  var templateTwo = (
   <div>
-    <h1>{user.name ? user.name : 'Anonymous'}</h1>
-    <p>Age: {user.age}</p>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1> {/*1 or 2 thing*/}
+    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>} {/*Do 1 thing* else nothing*/}
     {getLocation(user.location)}
   </div>
 );
