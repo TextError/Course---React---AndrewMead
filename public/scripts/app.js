5dc34b537121d5854891da086a5687b9cba90da0
unknown <savinraul1983@yahoo.com>
2018-09-30 16:39:39 +0100

    Challange

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,30 @@
-"use strict";
+'use strict';
 
-var squere = function squere(x) {
-  return x * x;
+// const squere = function(x) {
+//   return x * x
+// };
+
+// // const squereArrow = (x) => {
+// //   return x * x;
+// // };
+
+// const squereArrow = (x) => x * x;  //return single expresion
+
+// console.log(squereArrow(4));
+
+//Challenge
+var name = 'Raul Calin Savin';
+
+function middleName(name) {
+  return name.split(' ')[1];
+}
+
+var getFirstName = function getFirstName(name) {
+  return name.split(' ')[0];
 };
 
-var squereArrow = function squereArrow(x) {
-  return x * x;
+var getLastName = function getLastName(name) {
+  return name.split(' ')[2];
 };
 
-console.log(squereArrow(8));
+console.log(getLastName(name), getFirstName(name), middleName(name));
--- src/playground/es6-arrowFunction.js
+++ src/playground/es6-arrowFunction.js
@@ -1,9 +1,26 @@
-const squere = function(x) {
-  return x * x
-};
+// const squere = function(x) {
+//   return x * x
+// };
 
-const squereArrow = (x) => {
-  return x * x
+// // const squereArrow = (x) => {
+// //   return x * x;
+// // };
+
+// const squereArrow = (x) => x * x;  //return single expresion
+
+// console.log(squereArrow(4));
+
+//Challenge
+const name = 'Raul Calin Savin'
+
+function middleName(name) {
+  return name.split(' ')[1];
 }
 
-console.log(squereArrow(8));
+const getFirstName = (name) => {
+  return name.split(' ')[0];
+};
+
+const getLastName = (name) => name.split(' ')[2];
+
+console.log(getLastName(name), getFirstName(name), middleName(name));
