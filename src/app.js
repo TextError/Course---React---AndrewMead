1af9d226011a6d9a5cec1433051ae9a7e07c0ee3
unknown <savinraul1983@yahoo.com>
2018-10-01 16:44:00 +0100

    update count (like state)

--- src/app.js
+++ src/app.js
@@ -43,30 +43,34 @@ var template = (
 let count = 0;
 
 const addOne = () => {
-  console.log('clicked')
+  count++;   // count = count + 1;
+  renderCounterApp();
 };
 
 const minusOne = () => {
-  console.log('minusOne')
+  count--;
+  renderCounterApp();
 };
 
 const reset = () => {
-  console.log('reset')
+  count = 0;
+  renderCounterApp();
 };
-
-const templateTwo = (
-  <div>
-    <h1>Count: {count}</h1>
-    <button onClick={addOne}>+1</button>
-    <button onClick={minusOne}>-1</button>
-    <button onClick={reset}>reset</button>
-  </div>
-);
-
-//Challenge
-
-
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+
+const renderCounterApp = () => {
+  const templateTwo = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={addOne}>+1</button>
+      <button onClick={minusOne}>-1</button>
+      <button onClick={reset}>reset</button>
+    </div>
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
