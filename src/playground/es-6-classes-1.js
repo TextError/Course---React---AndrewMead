e448bf34b65d45f8d4cc2485c118928fd86f8196
unknown <savinraul1983@yahoo.com>
2018-10-02 11:48:19 +0100

    templet string

--- src/playground/es-6-classes-1.js
+++ src/playground/es-6-classes-1.js
@@ -2,10 +2,14 @@ class Person {
   constructor(name = 'Anonymus') {
     this.name = name;
   }
+  getGreeting() {
+    //return 'Hi ' + this.name + '!';
+    return `Hi ${this.name}!`
+  }
 };
 
 const me = new Person('Andrew Mead');
-console.log(me);
+console.log(me.getGreeting());
 
 const other = new Person();
-console.log(other);
+console.log(other.getGreeting());
