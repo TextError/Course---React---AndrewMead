3853a0b7b347b93df53e02e62f75fd422a17dfb1
unknown <savinraul1983@yahoo.com>
2018-10-03 16:51:57 +0100

    handle event

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -78,6 +78,11 @@ var Action = function (_React$Component3) {
   }
 
   _createClass(Action, [{
+    key: 'handlePick',
+    value: function handlePick() {
+      alert('handle click');
+    }
+  }, {
     key: 'render',
     value: function render() {
       return React.createElement(
@@ -85,7 +90,7 @@ var Action = function (_React$Component3) {
         null,
         React.createElement(
           'button',
-          null,
+          { onClick: this.handlePick },
           'What should I do?'
         )
       );
@@ -105,11 +110,21 @@ var Options = function (_React$Component4) {
   }
 
   _createClass(Options, [{
+    key: 'handleRemoveAll',
+    value: function handleRemoveAll() {
+      alert('remove all');
+    }
+  }, {
     key: 'render',
     value: function render() {
       return React.createElement(
         'div',
         null,
+        React.createElement(
+          'button',
+          { onClick: this.handleRemoveAll },
+          'Remove All'
+        ),
         this.props.options.map(function (option) {
           return React.createElement(Option, { option: option, key: option });
         })
--- src/app.js
+++ src/app.js
@@ -25,19 +25,30 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+
+  handlePick() {
+    alert('handle click')
+  };
+
   render() {
     return(
       <div>
-        <button>What should I do?</button>
+        <button onClick={this.handlePick}>What should I do?</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
+
+  handleRemoveAll() {
+    alert('remove all')
+  };
+
   render() {
     return(
       <div>
+        <button onClick={this.handleRemoveAll}>Remove All</button>
         {
           this.props.options.map((option) => <Option option={option} key={option}/>)
         }
