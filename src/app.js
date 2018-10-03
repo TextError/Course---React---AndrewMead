3c58c02cda10099a1ec4341f57e14bb3456951a4
unknown <savinraul1983@yahoo.com>
2018-10-03 17:37:18 +0100

    bind(this)

--- src/app.js
+++ src/app.js
@@ -40,9 +40,13 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleRemoveAll = this.handleRemoveAll.bind(this);
+  }
 
   handleRemoveAll() {
-    alert('remove all')
+    alert(this.props.options)
   };
 
   render() {
