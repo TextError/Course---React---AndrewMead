2e02c0782b7e33a3d6965f96c2befebce879eb00
unknown <savinraul1983@yahoo.com>
2018-10-11 11:38:05 +0100

    run webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
-console.log(__dirname)
+const path = require('path');
+
+// console.log(path.join(__dirname, 'public'));
 
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: './',
+    path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
   }
 };
